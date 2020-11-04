3477d729f9cfad56941af90e090456fde2bb5dc1
Thai Duong <htduong292001@gmail.com>
2020-11-05 01:35:34 +0800

    UndoCommand class: Add javadoc

--- src/main/java/jimmy/mcgymmy/logic/commands/UndoCommand.java
+++ src/main/java/jimmy/mcgymmy/logic/commands/UndoCommand.java
@@ -2,6 +2,9 @@ package jimmy.mcgymmy.logic.commands;
 
 import jimmy.mcgymmy.model.Model;
 
+/**
+ * Undo the previous action cause by other commands (except help).
+ */
 public class UndoCommand extends Command {
     public static final String COMMAND_WORD = "undo";
     public static final String SHORT_DESCRIPTION = "Undo the last command.";
